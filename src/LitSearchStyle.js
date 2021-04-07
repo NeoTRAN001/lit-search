80bdc361fe03e0262a9c59f4b6435713901b90ce
NeoTRAN001 <neo_tran@outlook.com>
2021-04-06 20:55:57 -0500

    change styles

--- src/LitSearchStyle.js
+++ src/LitSearchStyle.js
@@ -1,5 +1,18 @@
 import { css } from 'lit-element';
 
 export default css`
+    .container {
+        padding: 100px;
+        width: 100%;
+    }
 
+    input {
+        width: 600px;
+        height: 30px;
+        font-size: 20px;  
+    }
+
+    button {
+        height: 37px;
+    }
 `;
